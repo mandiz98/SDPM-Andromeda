6e7f7e7876f5ad39d5f5f3bec8dff2d5ab3c1891
mandiz98 <amanda.axelsson98@hotmail.com>
2019-09-13 10:01:31 +0200

    Added android app

---
+++ AndroidApp/app/src/main/java/se/ju/students/axam1798/andromeda/MainActivity.java
@@ -0,0 +1,13 @@
+package se.ju.students.axam1798.andromeda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
