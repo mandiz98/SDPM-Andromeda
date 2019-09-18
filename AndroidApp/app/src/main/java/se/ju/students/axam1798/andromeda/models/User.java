96cb572defce194443f2c9ffa2409dbede4d3986
Adam <a.clettborn@gmail.com>
2019-09-18 09:50:30 +0200

    Added radiation exposure unit per second function

--- AndroidApp/app/src/main/java/se/ju/students/axam1798/andromeda/models/User.java
+++ AndroidApp/app/src/main/java/se/ju/students/axam1798/andromeda/models/User.java
@@ -1,4 +1,15 @@
 package se.ju.students.axam1798.andromeda.models;
 
 public class User {
+
+    /**
+     * Calculate the current radiation exposure unit per second
+     * @param reactorRadiation Reactor radiation units output per second
+     * @param roomCoefficient The room's protective coefficient
+     * @param protectiveCoefficient Like clothes, hazmatsuite etc.
+     * @return current radiation exposure unit per second
+     */
+    public double getCurrentRadiationExposure(double reactorRadiation, double roomCoefficient, double protectiveCoefficient) {
+        return  (reactorRadiation * roomCoefficient) / protectiveCoefficient;
+    }
 }
