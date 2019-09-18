dac271f47edf1c1c95bff758631d48dc1440f915
Aleksandar Jelicic <Aleksandar.Jelicic@cybercom.com>
2019-09-18 10:34:03 +0200

    Changed from fragments to activities. Added clicklistener to Bt button to test changing views.

--- AndroidApp/app/src/main/java/se/ju/students/axam1798/andromeda/MainActivity.java
+++ AndroidApp/app/src/main/java/se/ju/students/axam1798/andromeda/MainActivity.java
@@ -1,17 +1,32 @@
 package se.ju.students.axam1798.andromeda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mTestBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTestBtn = (Button) findViewById(R.id.test_bt_btn);
+        mTestBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btConnected();
+            }
+        });
     }
 
-
-
-
+    private void btConnected(){
+        Intent intent = new Intent(this, ClockedIn.class);
+        startActivity(intent);
+    }
 }
