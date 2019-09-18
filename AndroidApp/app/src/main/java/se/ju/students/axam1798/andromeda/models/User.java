9158ec151bef3256bae1a5fcdc516e0c4128d91b
Adam <a.clettborn@gmail.com>
2019-09-18 13:37:29 +0200

    Modified User model to work with DB ORM library

--- AndroidApp/app/src/main/java/se/ju/students/axam1798/andromeda/models/User.java
+++ AndroidApp/app/src/main/java/se/ju/students/axam1798/andromeda/models/User.java
@@ -1,6 +1,30 @@
 package se.ju.students.axam1798.andromeda.models;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "users")
 public class User {
+    @DatabaseField(id = true)
+    private int id;
+
+    @DatabaseField(canBeNull = false)
+    private String rfid;
+
+    @DatabaseField()
+    private boolean clockedIn;
+
+    @DatabaseField()
+    private boolean hazmatSuite;
+
+    public User() {}
+
+    public User(int id, String rfid, boolean clockedIn, boolean hazmatSuite) {
+        this.id = id;
+        this.rfid = rfid;
+        this.clockedIn = clockedIn;
+        this.hazmatSuite = hazmatSuite;
+    }
 
     /**
      * Calculate the current radiation exposure unit per second
