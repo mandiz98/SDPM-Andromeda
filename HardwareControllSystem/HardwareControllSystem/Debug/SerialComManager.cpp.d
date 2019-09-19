841f70673604f77e9fde0c3fe94d62832163af25
Jimsan123 <44018567+Jimsan123@users.noreply.github.com>
2019-09-19 10:05:25 +0200

    added gitignores for visual studio and arduino

--- HardwareControllSystem/HardwareControllSystem/Debug/SerialComManager.cpp.d
+++
@@ -1,8 +0,0 @@
-C:\Users\Pontus\AppData\Local\Temp\VMBuilds\HardwareControllSystem\uno\Debug\SerialComManager.cpp.o: \
- C:\Users\Pontus\AppData\Local\Temp\VMBuilds\HardwareControllSystem\uno\Debug\SerialComManager.cpp \
- C:\Users\Pontus\AppData\Local\Temp\VMBuilds\HardwareControllSystem\uno\Debug\SerialComManager.h \
- D:\Arduino\ IDE\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
- D:\Arduino\ IDE\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
- D:\Arduino\ IDE\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
- D:\Arduino\ IDE\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
- D:\Arduino\ IDE\Arduino\hardware\arduino\avr\cores\arduino/Printable.h
