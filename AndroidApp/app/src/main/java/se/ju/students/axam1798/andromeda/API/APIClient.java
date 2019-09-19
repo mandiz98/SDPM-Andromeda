a63a390f6da4b2199f4e95b85405bcebbbf82a2a
BuildTools <a.clettborn@gmail.com>
2019-09-20 00:56:35 +0200

    Added API client

---
+++ AndroidApp/app/src/main/java/se/ju/students/axam1798/andromeda/API/APIClient.java
@@ -0,0 +1,54 @@
+package se.ju.students.axam1798.andromeda.API;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import java.io.IOException;
+import java.util.List;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import se.ju.students.axam1798.andromeda.models.User;
+
+public class APIClient {
+
+    private static String API_URL = "https://andromeda-rest-api.herokuapp.com";
+
+    private APIService apiService;
+    private static APIClient instance;
+
+    private APIClient() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(API_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        this.apiService = retrofit.create(APIService.class);
+    }
+
+    public static APIClient getInstance() {
+        if(instance == null)
+            instance = new APIClient();
+        return instance;
+    }
+
+    public void getUsers(Callback<List<User>> callback) {
+        this.apiService.getUsers().enqueue(callback);
+    }
+
+    public void createUser(User user, Callback<User> callback) {
+        this.apiService.createUser(user).enqueue(callback);
+    }
+
+    public static APIError decodeError(ResponseBody responseBody) {
+        Gson gson = new Gson();
+        try {
+            return gson.fromJson(responseBody.string(), APIError.class);
+        } catch (IOException e) {
+            return new APIError("Unknown", -1, "Error could not be parsed");
+        }
+    }
+}
