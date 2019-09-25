c2faf9adac42ba85088b73191b21f5212077295b
Adam <a.clettborn@gmail.com>
2019-09-25 14:34:52 +0200

    Added event and get user api endpoint

--- AndroidApp/app/src/main/java/se/ju/students/axam1798/andromeda/API/APIService.java
+++ AndroidApp/app/src/main/java/se/ju/students/axam1798/andromeda/API/APIService.java
@@ -5,8 +5,11 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.http.Body;
+import retrofit2.http.Field;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
+import se.ju.students.axam1798.andromeda.models.Event;
 import se.ju.students.axam1798.andromeda.models.User;
 
 public interface APIService {
@@ -14,7 +17,12 @@ public interface APIService {
     @GET("users")
     Call<List<User>> getUsers();
 
+    @GET("users/{id}")
+    Call<User> getUserById(@Path("id") int id);
+
     @POST("users")
     Call<User> createUser(@Body User user);
 
+    @POST("events")
+    Call<Event> createEvent(@Body Event event);
 }
