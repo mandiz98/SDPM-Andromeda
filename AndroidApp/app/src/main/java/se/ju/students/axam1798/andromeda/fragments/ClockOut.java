8826a79ee65e2f25615599ca60b0de521302694e
mandiz98 <amanda.axelsson98@hotmail.com>
2019-10-13 13:19:03 +0200

    Added ClockedOut text update

--- AndroidApp/app/src/main/java/se/ju/students/axam1798/andromeda/fragments/ClockOut.java
+++ AndroidApp/app/src/main/java/se/ju/students/axam1798/andromeda/fragments/ClockOut.java
@@ -1,8 +1,10 @@
 package se.ju.students.axam1798.andromeda.fragments;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
@@ -12,6 +14,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.util.Calendar;
 
 import se.ju.students.axam1798.andromeda.R;
 
@@ -20,25 +27,25 @@ public class ClockOut extends Fragment {
 
     private Button mOkBtn;
     final Handler mHandler = new Handler();
+    private View m_view = null;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_clock_out, container, false);
-
-            //Change back to main activity after a delay
-            mHandler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        okClicked();
-                    } catch(NullPointerException e){
-                        System.out.println("Caught the NullPointerException");
-                    }
+        m_view = inflater.inflate(R.layout.fragment_clock_out, container, false);
+        //Change back to main activity after a delay
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    okClicked();
+                } catch(NullPointerException e){
+                    System.out.println("Caught the NullPointerException");
                 }
-            }, 3000);
+            }
+        }, 5000);
 
-        mOkBtn = view.findViewById(R.id.ok_btn);
+        mOkBtn = m_view.findViewById(R.id.ok_btn);
 
         mOkBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,7 +53,10 @@ public class ClockOut extends Fragment {
                 okClicked();
             }
         });
-        return view;
+
+        timeClockedOut();
+
+        return m_view;
     }
     //Remove fragment and return to main activity
     private void okClicked() {
@@ -57,5 +67,30 @@ public class ClockOut extends Fragment {
             transaction.commit();
             fragmentManager.popBackStack();
 
+    }
+
+    private void timeClockedOut() {
+        String m_time;
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            m_time = java.time.LocalTime.now().toString();
+        } else {
+            Calendar calendar = Calendar.getInstance();
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+            m_time = sdf.format(calendar.getTime());
+        }
+
+        Activity main = getActivity();
+
+        if(main == null)
+            return;
+
+        final String time = m_time;
+        main.runOnUiThread(new Runnable() {
+           @Override
+            public void run() {
+               ((TextView)m_view.findViewById(R.id.txt_clocked_out_at_timer)).setText(time);
+            }
+        });
     }
 }
